79ced65b3ab43ef826ca025d8d897d9da76711f3
Blake X Wang <38148847+Yakumo-Ran@users.noreply.github.com>
2021-11-28 18:58:22 -0500

    Add files via upload

---
+++ visualization_for_SKnet.m
@@ -0,0 +1,31 @@
+% visualization for SKnet
+% 1. put this file and expRecord.csv in the same dictrionary
+% 2. run this code
+
+clear all
+
+% readCSV
+table = readtable('expRecord.csv');
+
+% extract data
+data = table2array(table(2:5, :));
+iteration = table2array(table(1, :));
+
+trainLoss = data(1,:);
+trainAcc = data(2,:);
+testLoss = data(3,:);
+testAcc = data(4,:);
+
+figure(1)
+plot(iteration, trainLoss, iteration, testLoss)
+title('loss vs epoch')
+legend('train loss','test loss')
+xlabel('epoch')
+ylabel('loss')
+
+figure(2)
+plot(iteration, trainAcc, iteration, testAcc)
+title('accurcy vs epoch')
+legend('train accurcy', 'test accurcy')
+xlabel('epoch')
+ylabel('accurcy')
